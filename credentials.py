7e21b6377c072beb5617db13e7e7c0976f2837e3
Dhruv Agarwal <dhruvagga@gmail.com>
2014-07-22 00:50:21 +0530

    template added

---
+++ credentials.py
@@ -0,0 +1,8 @@
+"""YOUR CREDENTIALS"""
+
+aws_access_key_id = ''
+aws_secret_access_key = ''
+region_name = 'ap-southeast-1'
+region_endpoint = 'sns.ap-southeast-1.amazonaws.com'
+platform_application_arn_android = '' #GCM Platform Application
+platform_application_arn_ios = '' #APNS Platform Application
